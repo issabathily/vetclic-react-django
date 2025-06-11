672b9ec8706785869e722865c4a1bb5b7d6e3778
B.Code/ <issabathily388@gmail.com>
2025-06-11 13:49:17 -0700

    Delete backend directory

--- backend/start_django.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Définir les variables d'environnement
-export DJANGO_SETTINGS_MODULE=vet_clinic.settings
-
-# Lancer le serveur Django
-echo "Démarrage du serveur Django..."
-python run.py
